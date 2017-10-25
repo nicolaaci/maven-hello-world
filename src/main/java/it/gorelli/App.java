41072dd3f9699be63beb28c410bf22e212b557bf
Gorelli Nicola (SOFTLAB SpA) <XSOF388@ACIINFO.LOCAL>
2017-10-25 17:27:36 +0200

    First commit

---
+++ src/main/java/it/gorelli/App.java
@@ -0,0 +1,11 @@
+package it.gorelli;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ){
+        System.out.println( "Hello from Nicola!" );
+    }
+}
